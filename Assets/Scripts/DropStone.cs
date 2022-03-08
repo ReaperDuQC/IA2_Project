0dc9546c7d9f780ac356dc04802d55803742e698
ReaperDuQC <kev_013@live.fr>
2022-03-08 16:03:35 -0500

    next submit with controller

---
+++ Assets/Scripts/DropStone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropStone : MonoBehaviour
+{
+   [SerializeField]
+   GameObject _stonePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
